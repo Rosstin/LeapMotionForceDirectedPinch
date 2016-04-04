a75762ddeab4b71c26e705b8e93d112ef50adf7f
Rosstin Murphy <asterazul@gmail.com>
2016-04-04 10:35:43 -0700

    you can magnet objects closer and farther

--- LeapMotionForceDirectedPinch/Assets/_SCRIPTS/MouseRaycast.cs
+++ LeapMotionForceDirectedPinch/Assets/_SCRIPTS/MouseRaycast.cs
@@ -27,19 +27,20 @@ public class MouseRaycast : MonoBehaviour {
 	private LeapPinchDetector rightPinchDetectorScript;
 	GameObject draggedObjectR = null;
 	float distanceOfDraggedObjectR = 0.0f;
+	float originalPinchDistanceR = 0.0f;
 
 	int stateL;
 	public GameObject leftPinchDetector;
 	private LeapPinchDetector leftPinchDetectorScript;
 	GameObject draggedObjectL = null;
 	float distanceOfDraggedObjectL = 0.0f;
+	float originalPinchDistanceL = 0.0f;
 
 	int STATE_NORMAL = 0;
 	int STATE_DRAGGING = 1;
 
 	int RIGHT = 0;
 	int LEFT = 1;
-
 
 	void Start () {
 		GameObject prefabLineToRender = Resources.Load("Line") as GameObject;
@@ -96,15 +97,18 @@ public class MouseRaycast : MonoBehaviour {
 			}
 		}
 
-		if (handedness == RIGHT) {
-			nodes [selectedNodeIndex].gameObject.GetComponent<MeshRenderer> ().material.color = Color.red;
-		} else {
-			nodes [selectedNodeIndex].gameObject.GetComponent<MeshRenderer> ().material.color = Color.green;
-		}
-
 
 		GameObject draggedObject = null;
 		float distanceOfDraggedObject = 0.0f;
+		float originalPinchDistance = 0.0f;
+
+		if (handedness == RIGHT) {
+			nodes [selectedNodeIndex].gameObject.GetComponent<MeshRenderer> ().material.color = Color.red; //getting component is slow
+			originalPinchDistance = originalPinchDistanceR;
+		} else {
+			nodes [selectedNodeIndex].gameObject.GetComponent<MeshRenderer> ().material.color = Color.green;
+			originalPinchDistance = originalPinchDistanceL;
+		}
 
 		int state = -1;
 		if( handedness == RIGHT){
@@ -122,10 +126,23 @@ public class MouseRaycast : MonoBehaviour {
 			state = STATE_DRAGGING;
 			draggedObject = nodes[selectedNodeIndex].gameObject;
 			distanceOfDraggedObject = Vector3.Distance (playerCamera.transform.position, draggedObject.transform.position);
+			originalPinchDistance = Vector3.Distance (playerCamera.transform.position, p);
 		}
 
 		if (state == STATE_DRAGGING) { // already dragging
 			Vector3 direction = heading / distanceOfDraggedObject;
+
+			float currentPinchDistance = Vector3.Distance (playerCamera.transform.position, p);
+
+			if (currentPinchDistance < originalPinchDistance * 0.90f ) {
+				// bring the object closer
+				distanceOfDraggedObject = distanceOfDraggedObject * 0.99f;
+			} else if (currentPinchDistance > originalPinchDistance * 1.10f ) {
+				distanceOfDraggedObject = distanceOfDraggedObject * 1.01f;
+			} else {
+				// do nothing
+			}
+
 			Vector3 objectPosition = playerCamera.transform.position + (direction.normalized * distanceOfDraggedObject);
 			draggedObject.transform.position = objectPosition;
 		}
@@ -138,11 +155,13 @@ public class MouseRaycast : MonoBehaviour {
 			stateR = state;
 			distanceOfDraggedObjectR = distanceOfDraggedObject;
 			draggedObjectR = draggedObject;
+			originalPinchDistanceR = originalPinchDistance;
 		}
 		else{
 			stateL = state;
 			distanceOfDraggedObjectL = distanceOfDraggedObject;
 			draggedObjectL = draggedObject;
+			originalPinchDistanceL = originalPinchDistance;
 		}
 
 		Vector3 endRayPosition = playerCamera.transform.position + (heading.normalized * 100.0f);
@@ -152,76 +171,6 @@ public class MouseRaycast : MonoBehaviour {
 		myLineRenderer.SetPosition (1, endRayPosition);
 		myLineRenderer.enabled = true;
 
-	}
-
-	/*
-	void LeapHandsPointerUpdate () {
-
-		// GET ACTIVITY -- are you pinching, clicking?
-		bool isActive = rightPinchDetectorScript.IsPinching;
-		bool activeThisFrame = rightPinchDetectorScript.DidStartPinch;
-
-		// GET POSITION OF EVENT
-		Vector3 p = rightPinchDetectorScript.Position;
-		//Vector3 p = pointerFingerTip.position;
-		//Vector3 p = globalMousePos;
-
-		Vector3 heading = p - playerCamera.transform.position;
-
-		// RAY STUFF
-
-		RaycastHit hit = new RaycastHit ();
-
-		//if (Physics.Raycast (playerCamera.transform.position, p, out hit)) { // if you hit something
-		if (Physics.SphereCast (playerCamera.transform.position, 12.0f, heading, out hit, 200.0f)) {
-			if (hit.transform.gameObject.tag == "Draggable") { // if it was a draggable object
-				hit.transform.gameObject.GetComponent<Draggable> ().OnHit ();
-				//if (Input.GetMouseButtonDown (0)) {
-				if(activeThisFrame){ // if you're pinching/clicking
-					state = STATE_DRAGGING;
-					draggedObject = hit.transform.gameObject;
-					distanceOfDraggedObject = Vector3.Distance (playerCamera.transform.position, hit.transform.gameObject.transform.position);
-				}
-			}
-		}
-
-
-		if (state == STATE_DRAGGING) {
-			Vector3 direction = heading / distanceOfDraggedObject;
-			Vector3 objectPosition = playerCamera.transform.position + (direction.normalized * distanceOfDraggedObject);
-			draggedObject.transform.position = objectPosition;
-		}
-
-		if (!isActive) {
-			state = STATE_NORMAL;
-		}
-
-		Vector3 endRayPosition = playerCamera.transform.position + (heading.normalized * 100.0f);
-
-		myLineRenderer.SetVertexCount (2);
-		myLineRenderer.SetPosition (0, p);
-		myLineRenderer.SetPosition (1, endRayPosition);
-		myLineRenderer.enabled = true;
-	
-	}*/
-
-	void MousePointerUpdate () {
-		// MOUSE POINTER STUFF
-		/*
-		var rt = pointer.GetComponent<RectTransform>();
-		Vector3 globalMousePos;
-		if (RectTransformUtility.ScreenPointToWorldPointInRectangle(canvas.GetComponent<RectTransform>(), new Vector2(Input.mousePosition.x, Input.mousePosition.y), playerCamera, out globalMousePos))
-		{
-			rt.position = globalMousePos;
-		}
-
-		if (Input.GetMouseButton(0)){
-			//Debug.Log ("mouse being held");
-			pointer.GetComponent<UnityEngine.UI.Image> ().sprite = Resources.Load<Sprite> ("pointerSpriteRed");
-		} else {
-			pointer.GetComponent<UnityEngine.UI.Image> ().sprite = Resources.Load<Sprite> ("pointerSprite");
-		}
-		*/
 	}
 
 
